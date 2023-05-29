d5c3f2acee7d95e107d742a9b8ac9e15fd5936d4
xrafas <xrafas@live.com>
2023-05-29 12:31:59 -0300

    Add files via upload

---
+++ nfse_detalhes.php
@@ -0,0 +1,305 @@
+<?php
+include("topo.php");
+
+date_default_timezone_set('America/Sao_Paulo');
+
+?>
+
+<div class="content well span12">
+    <div class="row">
+        <div class="span12">
+            <h2 class="text-center">Detalhes NFSe</h2>
+
+            <hr>
+
+            <div class="box-form-container">
+
+                <?php
+
+                $id_nota_fiscal = isset($_GET['id']) ? $_GET['id'] : die('ERROR: Record ID not found.');
+
+                $sql = "SELECT * FROM nfse WHERE Id = $id_nota_fiscal";
+                $result = querySQL($con, $sql);
+
+                echo '
+<form method="post">
+
+<input type="number" id="id_nota_fiscal" name="id_nota_fiscal" value="' . $id_nota_fiscal . '" readonly="true">
+
+<button id="editButton" type="button">Editar</button>
+<button id="saveButton" type="button" style="display:none;">Salvar</button>
+<br><br>
+
+    <ul>
+
+  
+
+        <li>
+            <label for="numero">Número:</label>
+            <input type="number" id="numero" name="numero" value="' . $result['Numero'] . '" readonly="true">
+        </li>
+        <li>
+            <label for="data_emissao">Data de Emissão:</label>
+            <input type="datetime-local" id="data_emissao" name="data_emissao" value="' . $result['Data_Emissao'] . '" readonly="true">
+        </li>
+        <li>
+            <label for="data_competencia">Data de Competência:</label>
+            <input type="datetime-local" id="data_competencia" name="data_competencia" value="' . $result['Data_Competencia'] . '" readonly="true">
+        </li>
+    </ul>
+    <br>
+
+    <b>Prestador:</b><br><br>
+    <ul>
+        <li>
+            <label for="prestador_cnpj">CNPJ:</label>
+            <input type="text" id="prestador_cnpj" name="prestador_cnpj" value="' . $result['Prestador_Cnpj'] . '" readonly="true">
+        </li>
+        <li>
+            <label for="prestador_razao_social">Razão Social:</label>
+            <input type="text" id="prestador_razao_social" name="prestador_razao_social" value="' . $result['Prestador_Razao_Social'] . '" readonly="true">
+        </li>
+        <li>
+            <label for="prestador_logradouro">Logradouro:</label>
+            <input type="text" id="prestador_logradouro" name="prestador_logradouro" value="' . $result['Prestador_Endereco_Logradouro'] . '" readonly="true">
+        </li>
+        <li>
+            <label for="prestador_numero">Número:</label>
+            <input type="text" id="prestador_numero" name="prestador_numero" value="' . $result['Prestador_Endereco_Numero'] . '" readonly="true">
+        </li>
+        <li>
+            <label for="prestador_bairro">Bairro:</label>
+            <input type="text" id="prestador_bairro" name="prestador_bairro" value="' . $result['Prestador_Endereco_Bairro'] . '" readonly="true">
+        </li>
+        <li>
+            <label for="prestador_codigo_municipio">Código do Município:</label>
+            <input type="text" id="prestador_codigo_municipio" name="prestador_codigo_municipio" value="' . $result['Prestador_Endereco_Codigo_Municipio'] . '" readonly="true">
+        </li>
+        <li>
+            <label for="prestador_uf">UF:</label>
+            <input type="text" id="prestador_uf" name="prestador_uf" value="' . $result['Prestador_Endereco_Uf'] . '" readonly="true">
+        </li>
+        <li>
+            <label for="prestador_cep">CEP:</label>
+            <input type="text" id="prestador_cep" name="prestador_cep" value="' . $result['Prestador_Endereco_Cep'] . '" readonly="true">
+        </li>
+
+    </ul>
+
+    <br>
+    <b>Tomador:</b><br><br>
+    <ul>
+        <li>
+            <label for="tomador_cpf">CPF:</label>
+            <input type="text" id="tomador_cpf" name="tomador_cpf" value="' . $result['Tomador_Cpf'] . '" readonly="true">
+        </li>
+        <li>
+            <label for="tomador_cnpj">CNPJ:</label>
+            <input type="text" id="tomador_cnpj" name="tomador_cnpj" value="' . $result['Tomador_Cnpj'] . '" readonly="true">
+        </li>
+        <li>
+            <label for="tomador_razao_social">Razão Social:</label>
+            <input type="text" id="tomador_razao_social" name="tomador_razao_social" value="' . $result['Tomador_Razao_Social'] . '" readonly="true">
+        </li>
+        <li>
+            <label for="tomador_logradouro">Logradouro:</label>
+            <input type="text" id="tomador_logradouro" name="tomador_logradouro" value="' . $result['Tomador_Endereco_Logradouro'] . '" readonly="true">
+        </li>
+        <li>
+            <label for="tomador_numero">Número:</label>
+            <input type="text" id="tomador_numero" name="tomador_numero" value="' . $result['Tomador_Endereco_Numero'] . '" readonly="true">
+        </li>
+        <li>
+            <label for="tomador_bairro">Bairro:</label>
+            <input type="text" id="tomador_bairro" name="tomador_bairro" value="' . $result['Tomador_Endereco_Bairro'] . '" readonly="true">
+        </li>
+        <li>
+            <label for="tomador_codigo_municipio">Código do Município:</label>
+            <input type="text" id="tomador_codigo_municipio" name="tomador_codigo_municipio" value="' . $result['Tomador_Endereco_Codigo_Municipio'] . '" readonly="true">
+        </li>
+        <li>
+            <label for="tomador_uf">UF:</label>
+            <input type="text" id="tomador_uf" name="tomador_uf" value="' . $result['Tomador_Endereco_Uf'] . '" readonly="true">
+        </li>
+        <li>
+            <label for="tomador_cep">CEP:</label>
+            <input type="text" id="tomador_cep" name="tomador_cep" value="' . $result['Tomador_Endereco_Cep'] . '" readonly="true">
+        </li>
+
+    </ul>
+
+    <b>Serviço:</b><br><br>
+    <ul>
+        <li>
+            <label for="servico_codigo">Código:</label>
+            <input type="text" id="servico_codigo" name="servico_codigo" value="' . $result['Servico_Codigo'] . '" readonly="true">
+        </li>
+        <li>
+            <label for="servico_codigo_cnae">Código CNAE:</label>
+            <input type="text" id="servico_codigo_cnae" name="servico_codigo_cnae" value="' . $result['Servico_Codigo_Cnae'] . '" readonly="true">
+        </li>
+        <li>
+            <label for="servico_codigo_tributacao_municipio">Código de Tributação do Município:</label>
+            <input type="text" id="servico_codigo_tributacao_municipio" name="servico_codigo_tributacao_municipio" value="' . $result['Servico_Codigo_Tributacao_Municipio'] . '" readonly="true">
+        </li>
+        <br>
+        <li>
+            <label for="servico_discriminacao">Discriminação:</label>
+            <textarea id="servico_discriminacao" name="servico_discriminacao" rows="4" cols="30" readonly="true">' . $result['Servico_Discriminacao'] . '</textarea>
+        </li>
+        <br>
+        <li>
+            <label for="servico_codigo_municipio">Código do Município:</label>
+            <input type="text" id="servico_codigo_municipio" name="servico_codigo_municipio" value="' . $result['Servico_Codigo_Municipio'] . '" readonly="true">
+        </li>
+        <li>
+            <label for="servico_codigo_municipio_prestacao">Código do Município de Prestação:</label>
+            <input type="text" id="servico_codigo_municipio_prestacao" name="servico_codigo_municipio_prestacao" value="' . $result['Servico_Codigo_Municipio'] . '" readonly="true">
+        </li>
+        <li>
+            <label for="servico_valor_servicos">Valor dos Serviços:</label>
+            <input type="text" id="servico_valor_servicos" name="servico_valor_servicos" value="' . $result['Servico_Valor_Servicos'] . '" readonly="true">
+        </li>
+        <li>
+            <label for="servico_valor_deducoes">Valor das Deduções:</label>
+            <input type="text" id="servico_valor_deducoes" name="servico_valor_deducoes" value="' . $result['Servico_Valor_Deducoes'] . '" readonly="true">
+        </li>
+        <li>
+            <label for="servico_valor_pis">Valor PIS:</label>
+            <input type="text" id="servico_valor_pis" name="servico_valor_pis" value="' . $result['Servico_Valor_Pis'] . '" readonly="true">
+        </li>
+        <li>
+            <label for="servico_valor_cofins">Valor COFINS:</label>
+            <input type="text" id="servico_valor_cofins" name="servico_valor_cofins" value="' . $result['Servico_Valor_Cofins'] . '" readonly="true">
+        </li>
+        <li>
+            <label for="servico_valor_inss">Valor INSS:</label>
+            <input type="text" id="servico_valor_inss" name="servico_valor_inss" value="' . $result['Servico_Valor_Inss'] . '" readonly="true">
+        </li>
+        <li>
+            <label for="servico_valor_ir">Valor IR:</label>
+            <input type="text" id="servico_valor_ir" name="servico_valor_ir" value="' . $result['Servico_Valor_Ir'] . '" readonly="true">
+        </li>
+        <li>
+            <label for="servico_valor_csll">Valor CSLL:</label>
+            <input type="text" id="servico_valor_csll" name="servico_valor_csll" value="' . $result['Servico_Valor_Csll'] . '" readonly="true">
+        </li>
+        <li>
+            <label for="servico_valor_outras">Valor Outras:</label>
+            <input type="text" id="servico_valor_outras" name="servico_valor_outras" value="' . $result['Servico_Valor_Outras'] . '" readonly="true">
+        </li>
+        <li>
+            <label for="servico_valor_iss">Valor ISS:</label>
+            <input type="text" id="servico_valor_iss" name="servico_valor_iss" value="' . $result['Servico_Valor_Iss'] . '" readonly="true">
+        </li>
+        <li>
+            <label for="servico_valor_aliquota">Valor Alíquota:</label>
+            <input type="text" id="servico_valor_aliquota" name="servico_valor_aliquota" value="' . $result['Servico_Valor_Aliquota'] . '" readonly="true">
+        </li>
+        <li>
+            <label for="servico_valor_desconto_condicionado">Valor do Desconto Condicionado:</label>
+            <input type="text" id="servico_valor_desconto_condicionado" name="servico_valor_desconto_condicionado" value="' . $result['Servico_Valor_Desconto_Condicionado'] . '" readonly="true">
+        </li>
+        <li>
+            <label for="servico_valor_desconto_incondicionado">Valor do Desconto Incondicionado:</label>
+            <input type="text" id="servico_valor_desconto_incondicionado" name="servico_valor_desconto_incondicionado" value="' . $result['Servico_Valor_Desconto_Incondicionado'] . '" readonly="true">
+        </li>
+        <li>
+            <label for="servico_exigibilidade_iss">Exigibilidade ISS:</label>
+            <input type="text" id="servico_exigibilidade_iss" name="servico_exigibilidade_iss" value="' . $result['Servico_Exigibilidade_Iss'] . '" readonly="true">
+        </li>
+        <li>
+            <label for="servico_iss_retido">ISS Retido:</label>
+            <input type="text" id="servico_iss_retido" name="servico_iss_retido" value="' . $result['Servico_Iss_Retido'] . '" readonly="true">
+        </li>
+
+    </ul>
+
+    <br>
+    Informações Complementares (Opcional):<br><br>
+
+    <li>
+        <textarea id="informacoes_complementares" name="informacoes_complementares" rows="3" cols="30" readonly="true">' . $result['Informacoes_Complementares'] . '</textarea>
+    </li>
+
+    </ul>
+
+    
+   
+
+</form>
+';
+
+                ?>
+
+            </div><!--CONTAINER-->
+
+        </div><!--Span-->
+
+    </div><!--Row-->
+
+</div><!--Content-->
+
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<script>
+    $(document).ready(function() {
+        $('#editButton').click(function() {
+            $('form input').removeAttr('readonly');
+            $('form textarea').removeAttr('readonly');
+            $('#editButton').hide();
+            $('#saveButton').show();
+        });
+
+        $('#saveButton').click(function() {
+            var confirmacao = confirm("Todos os dados da Nota Fiscal de Serviço Eletronica estão corretos?");
+
+            if (confirmacao == true) {
+                var formData = $(this).serialize();
+
+                $.ajax({
+                    url: 'nfse_edit_uptd_ajax.php',
+                    type: 'POST',
+                    data: formData,
+                    success: function(data) {
+
+                        console.log('sql_string: ' + data.sql_string);
+                        console.log('id_nota_fiscal: ' + data.id_nota_fiscal);
+
+                        if (data.error) {
+                            console.log('Erro no PHP: ' + data.error);
+                        }
+
+                        if (data.exists) {
+                            alert("O número de nota fiscal já existe.");
+                        } else if (data.saved) {
+                            //if (data.saved) {
+                            alert("Nota Fiscal salva com sucesso.");
+                            $('form input').attr('readonly', 'readonly');
+                            $('form textarea').attr('readonly', 'readonly');
+                            $('#saveButton').hide();
+                            $('#editButton').show();
+                            console.log(data);
+
+
+                        } else {
+                            alert("Houve um erro ao salvar a Nota Fiscal. Tente novamente.");
+                            console.log(data);
+
+                        }
+                    },
+                    error: function(jqXHR, textStatus, errorThrown) {
+                        console.log(jqXHR.responseText); // Mostrar a saída do script PHP
+                        console.log(textStatus, errorThrown);
+                        alert("Erro ao salvar a nota fiscal. Verifique com o suporte para mais detalhes.");
+                    }
+
+                });
+            } else {
+                // Se o usuário clicar em 'cancelar' na caixa de confirmação, não faça nada
+            }
+        });
+
+    }); //final do ready function.
+</script>
+
+<?php include("base.php"); ?>
---
+++ nfse_edit_uptd_ajax.php
@@ -0,0 +1,87 @@
+<?php
+header('Content-Type: application/json');
+
+include_once('session.php');
+include_once('../db_functions.php');
+$con = dbConnect();
+
+// decodificar os dados do formulário
+//$formData = json_decode(file_get_contents("php://input"), true);
+
+
+$numero = $_POST['numero'];
+
+$sql = "SELECT * FROM empresa WHERE Codigo = " . $_SESSION['codigo'] . " ";
+$result = querySQL($con, $sql);
+
+// verificar se o número da nota fiscal já existe
+$sql_nfse = "SELECT * FROM nfse WHERE Numero = " . $numero . " AND Id != " . $_POST["id_nota_fiscal"] . " AND Codigo = " . $_SESSION['codigo'] . " ";
+$notaExistente = querySQL($con, $sql_nfse);
+
+if ($notaExistente) {
+    echo json_encode(['exists' => true]);
+} else {
+    $sql = "UPDATE nfse SET ";
+
+    $sql .= "Empresa = '" . $result["Empresa"] . "', ";
+    $sql .= "Numero = '" . $_POST["numero"] . "', ";
+    $sql .= "Serie = '1', ";
+    $sql .= "Tipo = '1', ";
+    $sql .= "Status = '1', ";
+    $sql .= "Data_Emissao = '" . $_POST["data_emissao"] . "', ";
+    $sql .= "Data_Competencia = '" . $_POST["data_competencia"] . "', ";
+
+    $sql .= "Prestador_Cnpj = '" . $_POST["prestador_cnpj"] . "', ";
+    $sql .= "Prestador_Razao_Social = '" . $_POST["prestador_razao_social"] . "', ";
+    $sql .= "Prestador_Endereco_Logradouro = '" . $_POST["prestador_logradouro"] . "', ";
+    $sql .= "Prestador_Endereco_Numero = '" . $_POST["prestador_numero"] . "', ";
+    $sql .= "Prestador_Endereco_Bairro = '" . $_POST["prestador_bairro"] . "', ";
+    $sql .= "Prestador_Endereco_Codigo_Municipio = '" . $_POST["prestador_codigo_municipio"] . "', ";
+    $sql .= "Prestador_Endereco_Uf = '" . $_POST["prestador_uf"] . "', ";
+    $sql .= "Prestador_Endereco_Cep = '" . $_POST["prestador_cep"] . "', ";
+    $sql .= "Servico_Codigo_Tributacao_Municipio = '" . $_POST["servico_codigo_tributacao_municipio"] . "', ";
+    $sql .= "Servico_Discriminacao = '" . $_POST["servico_discriminacao"] . "', ";
+    $sql .= "Servico_Codigo_Municipio = '" . $_POST["servico_codigo_municipio"] . "', ";
+    $sql .= "Servico_Valor_Servicos = '" . $_POST["servico_valor_servicos"] . "', ";
+    $sql .= "Servico_Valor_Aliquota = '" . $_POST["servico_valor_aliquota"] . "', ";
+
+    $sql .= "Tomador_Cpf = '" . $_POST["tomador_cpf"] . "', ";
+    $sql .= "Tomador_Cnpj = '" . $_POST["tomador_cnpj"] . "', ";
+    $sql .= "Tomador_Razao_Social = '" . $_POST["tomador_razao_social"] . "', ";
+    $sql .= "Tomador_Endereco_Logradouro = '" . $_POST["tomador_logradouro"] . "', ";
+    $sql .= "Tomador_Endereco_Numero = '" . $_POST["tomador_numero"] . "', ";
+    $sql .= "Tomador_Endereco_Bairro = '" . $_POST["tomador_bairro"] . "', ";
+    $sql .= "Tomador_Endereco_Codigo_Municipio = '" . $_POST["tomador_codigo_municipio"] . "', ";
+    $sql .= "Tomador_Endereco_Uf = '" . $_POST["tomador_uf"] . "', ";
+    $sql .= "Tomador_Endereco_Cep = '" . $_POST["tomador_cep"] . "', ";
+    $sql .= "Servico_Codigo = '" . $_POST["servico_codigo"] . "', ";
+    $sql .= "Servico_Codigo_Cnae = '" . $_POST["servico_codigo_cnae"] . "', ";
+    $sql .= "Servico_Codigo_Municipio_Prestacao = '" . $_POST["servico_codigo_municipio_prestacao"] . "', ";
+    $sql .= "Servico_Valor_Deducoes = '" . $_POST["servico_valor_deducoes"] . "', ";
+    $sql .= "Servico_Valor_Pis = '" . $_POST["servico_valor_pis"] . "', ";
+    $sql .= "Servico_Valor_Cofins = '" . $_POST["servico_valor_cofins"] . "', ";
+    $sql .= "Servico_Valor_Inss = '" . $_POST["servico_valor_inss"] . "', ";
+    $sql .= "Servico_Valor_Ir = '" . $_POST["servico_valor_ir"] . "', ";
+    $sql .= "Servico_Valor_Csll = '" . $_POST["servico_valor_csll"] . "', ";
+    $sql .= "Servico_Valor_Outras = '" . $_POST["servico_valor_outras"] . "', ";
+    $sql .= "Servico_Valor_Iss = '" . $_POST["servico_valor_iss"] . "', ";
+    $sql .= "Servico_Valor_Desconto_Condicionado = '" . $_POST["servico_valor_desconto_condicionado"] . "', ";
+    $sql .= "Servico_Valor_Desconto_Incondicionado = '" . $_POST["servico_valor_desconto_incondicionado"] . "', ";
+    $sql .= "Servico_Exigibilidade_Iss = '" . $_POST["servico_exigibilidade_iss"] . "', ";
+    $sql .= "Servico_Iss_Retido = '" . $_POST["servico_iss_retido"] . "', ";
+    $sql .= "Informacoes_Complementares = '" . $_POST["informacoes_complementares"] . "' ";
+    //$sql .= "Obra_Codigo = '" . $_POST["Obra_Codigo"] . "', ";
+    //$sql .= "Obra_Art = '" . $_POST["Obra_Art"] . "', ";
+    //$sql .= "Chave = '" . $_POST["Chave"] . "', ";
+    $sql .= " WHERE Id = '" . $_POST["id_nota_fiscal"] . "'";
+    //$sql .= " WHERE Id = '" . $_POST["id_nota_fiscal"] . "' AND Codigo = '" . $_SESSION['codigo'] . "'";
+
+    // Armazenando a string SQL na variável de sessão
+    $_SESSION['sql_string'] = $sql;
+
+    if (execSQL($con, $sql)) {
+        echo json_encode(['saved' => true, 'sql_string' => $_SESSION['sql_string'], 'id_nota_fiscal' => $_POST["id_nota_fiscal"]]);
+    } else {
+        echo json_encode(['saved' => false, 'error' => mysqli_error($con)]);
+    }
+}
